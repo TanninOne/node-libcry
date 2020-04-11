34095f189291523bff93d6f2dbd8ed8f8b9ef530
TanninOne <seppleviathan@gmx.de>
2020-04-11 16:08:24 +0200

    completed first sample

--- packages/node-libcrypak/index.d.ts
+++ packages/node-libcrypak/index.d.ts
@@ -1,3 +1,3 @@
 export function decrypt(src: string, dest: string, key: string): void;
-export function decryptFile(src: string, key: string, fileNames: string[]): Buffer[];
+export function decryptFiles(src: string, key: string, fileNames: string[]): Buffer[];
 export function listFiles(src: string, key: string): string[];
--- packages/pakdec/src/index.ts
+++ packages/pakdec/src/index.ts
@@ -1,3 +1,30 @@
-import { listFiles } from 'crypak';
+import * as cp from 'crypak';
+import * as cx from 'cryxml';
+import * as fs from 'fs';
+import * as path from 'path';
 
-listFiles(process.argv[2], 'deadbeef');
+export function main(pakPath: string, keyPath: string) {
+    const wolcenkey = fs.readFileSync(keyPath).toString('hex');
+
+    // list all files in the Umbra.pak file
+    const files = cp.listFiles(pakPath, wolcenkey);
+    console.log(`pak contains ${files.length} files`);
+
+    // pick a random xml file from the pak
+    const xmlFiles = files.filter(name => path.extname(name) === '.xml');
+    const pos = Math.floor(Math.random() * xmlFiles.length);
+    console.log('picking', xmlFiles[pos]);
+    const binBuf = cp.decryptFiles(pakPath, wolcenkey, xmlFiles.slice(pos, pos + 1));
+
+    // assuming files aren't compressed, ignoring the header completely
+    const offset = String.fromCharCode(...binBuf[0]).indexOf('CryXmlB');
+    const data = binBuf[0].slice(offset);
+
+    // decode the binary xml
+    const xml = cx.decodeBuffer(data);
+    console.log(xml);
+}
+
+if (require.main === module) {
+    main(process.argv[2], process.argv[3]);
+}
